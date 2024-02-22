fb6d6b2bf175ce969fd616ed92ea90e001537b40
Avery <avery.cantrell@gmail.com>
2024-02-22 18:07:12 -0500

    Settings component logic

--- public/tetrisClient/components/settingsMenu.tsx
+++ public/tetrisClient/components/settingsMenu.tsx
@@ -1,49 +1,58 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-/*
-
-<p class="category">Local Settings</p>
-    <label for="usernameSetting">Username: </label>
-        <input type=text" id="usernameSetting" readonly> <input type="button" id="newUsernameButton" value="New"></input>
-    <br>
-    <label for="autorepeatDelay">Key Autorepeat Delay (ms):</label> 
-        <input type="number" required minlength="1" id="autorepeatDelay"></input>
-    <br>
-    <label for="autorepeatSpeed">Key Autorepeat Speed (ms):</label> 
-        <input type="number" required minlength="1" min="1" id="autorepeatSpeed"></input>
-
-*/
-
 // a setting in the settings form
-const settingField = ({ setting, label, props, readonly, onClick }) => {
-    return (
-        <br>
-            <label for={setting}>{label} </label>
-            <input id={setting} readonly={readonly} {...props} />
-            {readonly ? (<input type="button" id={`new${setting}`} value="New" onClick={onClick}></input>) : ''}
-        </br>
-    )
-}
+const settingField = ({ setting, label, props, readonly, onClick }) => (
+    <br>
+        <label for={setting}>{label} </label>
+        <input id={setting} readonly={readonly} {...props} />
+        {readonly ? (<input type="button" id={`new${setting}`} value="New" onClick={onClick}></input>) : ''}
+    </br>
+)
 
 // collection of setting fields
-const settingCategory = ({ title, children }) => {
-    return (
+const settingCategory = ({ title, settingMap }) => {
+    let settings: HTMLElement[] = [];
+    for(let [setting, details] of settingMap) {
+        settings.push(
+            <settingField   
+                setting={setting} 
+                label={details.label}
+                props={details.props}
+                readonly={details.readonly}
+                onClick={details.onClick}
+            />
+        );
+    }
+
+    return(
         <br>
-            <p class="category">{title}</p>
-                { children }
+        <p class="category">{title}</p>
+            {settings}
         </br>
-    )
+    );
 }
 
+
 // settings form; categoryMap: map of default settings, readonlyMethods: methods for read only settings' buttons, onReset: method called when reset settings button is clicked, onSave: method called when save button is clicked
-export const SettingsMenu = ({ categoryMap, readonlyMethods, onReset, onSave }) => {
+export const SettingsMenu = ({ categoryMap, onReset, onSave }) => {
+    let categories: HTMLElement[] = [];
+    for(let [category, settings] of categoryMap) {
+        categories.push(
+            <settingCategory 
+                title={category} 
+                settingMap={settings} 
+            />
+        );
+    }
+
+
     return (
         <div>
             <span class="close">&times;</span>
             <form id="settingsForm">
 
-
+            {categories}
 
             <input class="buttons" id="resetButton" type="button" value="Reset Defaults" />
             <input class="buttons" type="submit" value="Save" />
